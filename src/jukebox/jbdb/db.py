2609d6910ddc23b0ca60b1dcf4691df40c68da26
Thomas Hrabe <thrabe@gmail.com>
2020-03-26 18:41:50 +0000

    first db steps

--- src/jukebox/jbdb/db.py
+++ src/jukebox/jbdb/db.py
@@ -1,9 +1,17 @@
 import json
 
-def read_jbdb(db_file_path):
+def read_jbdb(db_file_path: str):
     """
-    
+    Reads JukeBox db file
+    :param db_file_path:
     """
+
+    with open(db_file_path, 'r') as f:
+        jbdb = json.load(f)
+
+    return jbdb
+
+
 
 
 
