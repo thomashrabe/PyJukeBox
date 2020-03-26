0879bdcda852e2c175ad0d45c124cc786694f7af
Thomas Hrabe <thrabe@gmail.com>
2020-03-26 23:04:32 +0000

    source

--- bin/jukebox.sh
+++ bin/jukebox.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# http://blog.scphillips.com/posts/2013/07/getting-a-python-script-to-run-in-the-background-as-a-service-on-boot/
+
 HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 JUKEBOX_SRC=$HERE/../src
 export PYTHONPATH=$JUKEBOX_SRC:$PYTHONPATH
