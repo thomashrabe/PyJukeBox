4d56950f9c333e6414582386b31c27d7f27f9ff6
Thomas Hrabe <thrabe@gmail.com>
2020-04-09 23:54:42 +0100

    run init on install #17

--- bin/install.sh
+++ bin/install.sh
@@ -5,10 +5,5 @@ sudo apt install evtest
 sudo pip3 install evdev
 sudo pip3 install python-vlc
 
-mkdir -p /home/pi/.jukebox
-touch /home/pi/.jukebox/db.jdbd
-
-echo 'Edit crontab and add '
-echo '@reboot /home/pi/jukebox/bin/jukebox.sh > /home/pi/.jukebox/jukebox.log 2> /home/pi/.jukebox/jukebox.err'
-
+python3 ./../src/jukebox/jukebox.py --init --db ~/.jukebox/db.jdbd
 
