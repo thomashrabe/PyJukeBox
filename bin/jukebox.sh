00c12b683b2e1a449cda933f5e839d892358a256
Thomas Hrabe <thrabe@gmail.com>
2020-12-12 17:27:57 -0800

    filename typo #28

--- bin/jukebox.sh
+++ bin/jukebox.sh
@@ -6,4 +6,4 @@ HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 JUKEBOX_SRC=$HERE/../src
 export PYTHONPATH=$JUKEBOX_SRC:$PYTHONPATH
 
-$JUKEBOX_SRC/jukebox/jukebox.py --db /home/pi/.jukebox/db.jdbd
+$JUKEBOX_SRC/jukebox/jukebox.py --db /home/pi/.jukebox/db.jbdb
