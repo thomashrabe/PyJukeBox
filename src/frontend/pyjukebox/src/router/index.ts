4770246b693f24dce03910087c80d8fff7606c27
Thomas Hrabe <thrabe@gmail.com>
2020-12-22 15:24:51 -0800

    indentation #30

--- src/frontend/pyjukebox/src/router/index.ts
+++ src/frontend/pyjukebox/src/router/index.ts
@@ -6,23 +6,23 @@ import Main from '../views/Main.vue'
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
-  {
-    path: '/',
-    name: 'Main',
-    component: Main
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    {
+        path: '/',
+        name: 'Main',
+        component: Main
+    },
+    {
+        path: '/about',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    }
 ]
 
 const router = new VueRouter({
-  routes
+    routes
 })
 
 export default router
