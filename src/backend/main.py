c5b4913a7ed62d839c5ab8550c04c6b939db2f49
Thomas Hrabe <thrabe@gmail.com>
2020-12-22 15:24:21 -0800

    enable fastapi cors #30

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python3
 
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 
 from jukebox.db import read_jbdb
 
 jukeboxBackend = FastAPI()
+
+origins = [
+    "http://localhost:8080"
+]
+
+jukeboxBackend.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @jukeboxBackend.get("/")
 async def root():
