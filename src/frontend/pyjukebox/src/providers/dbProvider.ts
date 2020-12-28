1f10657c6aed55f8347ab68ccbbf312573f3787f
Thomas Hrabe <thrabe@gmail.com>
2020-12-28 11:56:41 -0800

    db provider changed #30

--- src/frontend/pyjukebox/src/providers/dbProvider.ts
+++ src/frontend/pyjukebox/src/providers/dbProvider.ts
@@ -4,8 +4,20 @@ export class DBProvider {
         baseURL: "http://localhost:8000"
     }
 
+    /**
+     * Request DB JSON from backend
+     */
     static requestDB() {
         const client = axios.create(DBProvider.config);
         return client.get("/db");
+    }
+
+    /**
+     * Call the backend to create a new folder
+     * @param newFolderName New folder name
+     */
+    static createNewFolder(newFolderName: string){
+        const client = axios.create(DBProvider.config);
+        return client.post("/addFolder/" + newFolderName);
     }
 }
